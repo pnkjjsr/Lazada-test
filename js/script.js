2abd3d390b8eb7a4e1c4b12a922a861762da8c2d
pnkjjsr <pnkj_jsr@yahoo.co.in>
2016-09-07 14:04:16 +0530

    Tab active class changes.

--- js/script.js
+++ js/script.js
@@ -10,11 +10,26 @@ var objLightBox = new lightBox();
 //Tabs Object
 var tabs = function () {
     this.tab = function (el) {
-        var getEl = document.getElementsByClassName('tabs')[0].getElementsByClassName('container')[0].getElementsByClassName('tab');
-        for (var i = 0; i < getEl.length; i += 1) {
-            getEl[i].style.display = "none";
+
+//        Tab Active Class Changes
+        var getTab = document.getElementsByClassName('tabs')[0].getElementsByClassName('tab')[0].getElementsByTagName('li');
+        for (var i = 0; i < getTab.length; i += 1) {
+            getTab[i].classList.remove("active");
         }
-        getEl = document.getElementsByClassName(el)[0].style.display = "block";
+
+        var target;
+        document.addEventListener('click', function (e) {
+            e = e || window.event;
+            target = e.target || e.srcElement;
+            target.classList.add("active");
+        });
+
+//        Tab Container Changes
+        var getContainer = document.getElementsByClassName('tabs')[0].getElementsByClassName('container')[0].getElementsByClassName('tab');
+        for (var i = 0; i < getContainer.length; i += 1) {
+            getContainer[i].style.display = "none";
+        }
+        getContainer = document.getElementsByClassName(el)[0].style.display = "block";
     };
 };
 var objTabs = new tabs();
